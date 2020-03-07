f4055706ab4c8e154624e129ff1cc35002631fdf
Andres <acja87@hotmail.com>
2020-03-07 11:06:41 -0500

    se hace la suma de dos espación de texto

--- Scripts/calc.js
+++ Scripts/calc.js
@@ -1,5 +1,14 @@
+var numero1 = null;
+var numero2 = null;
+
 function suma() {
-  var numero1 = +document.getElementById('text1').value;
-  var numero2 = +document.getElementById('text2').value;
+  // var numero1 = +document.getElementById('text1').value;
+  a();
   var resultado = document.getElementById('resultado');
+  resultado.innerHTML = 'Resultado: ' + (+numero1.value + +numero2.value);
 }
+
+a = function() {
+  numero1 = document.getElementById('text1');
+  numero2 = document.getElementById('text2');
+};
