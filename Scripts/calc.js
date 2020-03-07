98e75960140085b452d47cb62be4f23a55f04932
Andres <acja87@hotmail.com>
2020-03-07 09:16:38 -0500

    base uso de funciones

---
+++ Scripts/calc.js
@@ -0,0 +1,5 @@
+function suma() {
+  var numero1 = +document.getElementById('text1').value;
+  var numero2 = +document.getElementById('text2').value;
+  var resultado = document.getElementById('resultado');
+}
